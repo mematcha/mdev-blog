921c7851609f3a64e72eb94bb2d942a0e1b70638
mematcha <sathwikmatcha.1899@gmail.com>
2023-10-16 20:03:15 -0400

    Created BlogPage JS

---
+++ src/pages/blogPage.js
@@ -0,0 +1,16 @@
+import MainHeader from "../components/mainHeader";
+
+function BlogPage(){
+    return (
+    <>
+        <div className="flex flex-col">
+            <MainHeader></MainHeader>
+            <div>
+                
+            </div>
+        </div>
+    </>
+    );
+}
+
+export default BlogPage;
