0c891ad405535280ff7929fe1636ba326697d882
mematcha <sathwikmatcha.1899@gmail.com>
2023-10-17 00:40:38 -0400

    Created User and CreateBlog Page

--- src/pages/createBlog.js
+++ src/pages/createBlog.js
@@ -1,15 +1,14 @@
 import MainHeader from "../components/mainHeader";
 
 
-function UserPage() {
+function CreateBlog() {
   return (
     <>
       <div className="body-center fixed flex justify-center top-0 left-0 shadow bg-white z-10">
         <MainHeader></MainHeader>
       </div>
-      Hi, Matcha!
     </>
   );
 }
 
-export default UserPage;
+export default CreateBlog;
