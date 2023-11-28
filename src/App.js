568a2ef3a6f7e4e0de9e0c75405d46f35490c948
mematcha <sathwikmatcha.1899@gmail.com>
2023-11-27 23:33:00 -0500

    Created Basic About Me Page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { DeviceContext, DeviceProvider } from "./DeviceContext";
 import HomePage from "./pages/homePage";
 import BlogPage from "./pages/blogPage";
 import UserPage from "./pages/userPage";
+
 import CreateBlog from "./pages/createBlog";
 import { ThemeContext, ThemeProvider } from "./ThemeContext";
+import AboutMe from "./pages/AboutMe";
 // import socialCard from './components/'
 
 function App() {
@@ -24,6 +26,7 @@ function App() {
             <Route path="/" element={<HomePage></HomePage>}></Route>
             <Route path="/page" element={<BlogPage />} />
             <Route path="/user" element={<UserPage></UserPage>}></Route>
+            <Route path="/about-me" element={<AboutMe></AboutMe>}></Route>
             <Route
               path="/create-blog"
               element={<CreateBlog></CreateBlog>}
