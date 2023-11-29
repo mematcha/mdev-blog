c0beda799a5eb5c12182983a16d1624dfc095d33
mematcha <sathwikmatcha.1899@gmail.com>
2023-11-29 01:44:37 -0500

    Created a Basic Carousel Component with Library

--- src/components/skils_carousel/skillsCarousel.js
+++ src/components/skils_carousel/skillsCarousel.js
@@ -1,7 +1,35 @@
-function SkillsCarousel(){
-    return(
-        <>
-        </>
-    );
+import { useEffect, useState } from "react";
+import Slider from 'react-slick';
+
+function SkillsCarousel() {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const [skills,setSkills]=useState();
+
+  const intializeSkills = ()=>{
+    setSkills(["Javascript","Typescript","Angular"]);
+  };
+
+  useEffect(()=>{
+    intializeSkills();
+  },[]);
+
+
+  return (
+    <Slider {...settings}>
+      {/* {skills.map((skill, index) => (
+        <div key={index}>
+          <li></li>
+        </div>
+      ))} */}
+    </Slider>
+  );
 }
 export default SkillsCarousel;
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,4 +1,5 @@
 import MainHeader from "../components/mainHeader";
+import SkillsCarousel from "../components/skils_carousel/skillsCarousel";
 
 function AboutMe() {
   return (
@@ -9,7 +10,7 @@ function AboutMe() {
           <div className="mb-4">
             <div className="mb-4 font-bold text-[24px]">About Me</div>
             <div>
-              <p>Hi! Sathwik over here.</p>
+              <p>Hi! Sathwik Matcha over here. You can call me just Sathwik. 😊</p>
               <br></br>
               <p>
                 I am a ML Enthusiast with a knack for Software Development, with
@@ -22,11 +23,35 @@ function AboutMe() {
               </p>
             </div>
           </div>
-          <div className="mb-4 font-bold text-[24px]">Technical Skills</div>
-          <div></div>
-          <div className="mb-4 font-bold text-[24px]">Why MDev.Blog?</div>
-          <div className="mb-4 font-bold text-[24px]">Resume</div>
-          <div className="mb-4 font-bold text-[24px]">Contact</div>
+          <div className="mb-4">
+            <div className="mb-4 font-bold text-[24px]">Technical Skills</div>
+            <div>
+              <SkillsCarousel></SkillsCarousel>
+            </div>
+          </div>
+          <div className="mb-4">
+            <div className="mb-4 font-bold text-[24px]">Why MDev.Blog?</div>
+            <div>
+              <p>
+                The main mission of this blog is to be a proper presentation of
+                knowledge rather than a simple mumble jumble of the latest
+                happenings in a space. The gap between what we want to learn and
+                what is being presented has no correlation in the modern social
+                media space. I hope this website bridges the gap between the
+                two.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="mb-4 font-bold text-[24px]">Resume</div>
+            <div className="p-4 border-2 rounded">View Resume</div>
+          </div>
+          <div>
+            <div className="mb-4 font-bold text-[24px]">Contact</div>
+            <div>Email</div>
+            <div>LinkedIn</div>
+            <div>Twitter</div>
+          </div>
         </div>
       </div>
     </>
