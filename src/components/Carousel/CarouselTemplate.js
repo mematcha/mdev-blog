e5bd855d65754badf7f259d58be1c9341b581ac6
mematcha <sathwikmatcha.1899@gmail.com>
2024-01-23 09:07:22 -0500

    Carousel Template Changes

--- src/components/Carousel/CarouselTemplate.js
+++ src/components/Carousel/CarouselTemplate.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../ThemeContext";
 function CarouselTemplate({data}){
+    const {theme} = useContext(ThemeContext);
+
     return (
-        <div className="bg-white rounded-[10px]">
+        <div className=" rounded-[10px]">
             {/* header element */}
-            <div className="p-2 bg-slate-200 rounded-t-[10px]">
+            <div className={`p-2 rounded-t-[10px] ${theme=="dark"?"bg-gray-600":"bg-slate-200"}`}>
                 {data.title}
             </div>
             <div>
