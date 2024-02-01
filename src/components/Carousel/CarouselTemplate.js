3893dccfc0ac70538b3f180cc5c646902f6e0ff1
mematcha <sathwikmatcha.1899@gmail.com>
2024-02-01 13:01:24 -0500

    About Me Changes

--- src/components/Carousel/CarouselTemplate.js
+++ src/components/Carousel/CarouselTemplate.js
@@ -1,23 +1,29 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../ThemeContext";
-function CarouselTemplate({data}){
-    const {theme} = useContext(ThemeContext);
+function CarouselTemplate({ data }) {
+  const { theme } = useContext(ThemeContext);
 
-    return (
-        <div className=" rounded-[10px]">
-            {/* header element */}
-            <div className={`p-2 rounded-t-[10px] ${theme=="dark"?"bg-gray-600":"bg-slate-200"}`}>
-                {data.title}
+  return (
+    <div className=" rounded-[10px]">
+      {/* header element */}
+      <div
+        className={`p-2 rounded-t-[10px] ${
+          theme == "dark" ? "bg-gray-600" : "bg-slate-200"
+        }`}
+      >
+        {data.title}
+      </div>
+      <div>
+        <div className="grid-container">
+          {data.courses.map((point, index) => (
+            <div className="grid-item">
+              <a href={point.url} target="_blank">{point.text}</a>
             </div>
-            <div>
-                <div className="grid-container">
-                    {data.courses.map((point,index)=>(
-                        <div className="grid-item">{point}</div>
-                    ))}
-                </div>
-            </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CarouselTemplate;
